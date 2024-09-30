e254e3126d340c1e5c64ce7bc47b05a1ed8c1a3a
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-30 21:24:21 +0530

    Update 0_AADI_GOLGAPPE.js

--- Priyansh/commands/0_AADI_GOLGAPPE.js
+++ Priyansh/commands/0_AADI_GOLGAPPE.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if (event.body.indexOf("Golgappe")==0 || event.body.indexOf("Golgappa")==0 || event.body.indexOf("Pani puri")==0 || event.body.indexOf("PANI PURI")==0) {
 		var msg = {
 				body: "😋 𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐆𝐎𝐋𝐆𝐀𝐏𝐏𝐀 𝐊𝐇𝐀𝐎 😋",
-				attachment: fs.createReadStream(__dirname + `/AADI/AADI3.jpg`)
+				attachment: fs.createReadStream(__dirname + `/noprefix/AADI3.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😋", event.messageID, (err) => {}, true)
