6ad85411074c3149942e7690d0a69f79aaec55cc
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-30 21:24:46 +0530

    Update 0_AADI_KHANA.js

--- Priyansh/commands/0_AADI_KHANA.js
+++ Priyansh/commands/0_AADI_KHANA.js
@@ -20,7 +20,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("kuch khaya") ||
 react.includes("kha lo")) {
 		var msg = {
-				body: `🙂 आओ बेबी मेरे साथ खा लो खाना 🙂`,attachment: fs.createReadStream(__dirname + `/AADI/AA2.jpg`)
+				body: `🙂 आओ बेबी मेरे साथ खा लो खाना 🙂`,attachment: fs.createReadStream(__dirname + `/noprefix/AA2.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍲", event.messageID, (err) => {}, true)
