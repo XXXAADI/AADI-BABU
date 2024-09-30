b991d5ea22887bb771a3358dfc47d2061cbd5817
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-30 20:46:05 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -1,62 +1,94 @@
+/**
+ * @author MintDaL
+ * @warn Do not edit code or edit credits
+ */
+
 module.exports.config = {
-	name: "info",
-	version: "1.0.1", 
-	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-	description: "Admin and Bot info.",
-	commandCategory: "...",
-	cooldowns: 1,
-	dependencies: 
-	{
-    "request":"",
-    "fs-extra":"",
-    "axios":""
+  name: "info",
+  version: "1.2.6",
+  hasPermssion: 0,
+  credits: "kensu",
+  description: "info bot owner",
+  commandCategory: "Dành cho người dùng",
+  hide:true,
+  usages: "",
+  cooldowns: 5,
+};
+
+
+module.exports.run = async function ({ api, event, args, Users, permssion, getText ,Threads}) {
+  const content = args.slice(1, args.length);
+  const { threadID, messageID, mentions } = event;
+  const { configPath } = global.client;
+  const { ADMINBOT } = global.config;
+   const { NDH } = global.config;
+  const { userName } = global.data;
+  const request = global.nodemodule["request"];
+  const fs = global.nodemodule["fs-extra"];
+  const { writeFileSync } = global.nodemodule["fs-extra"];
+  const mention = Object.keys(mentions);
+  delete require.cache[require.resolve(configPath)];
+  var config = require(configPath);
+  const listAdmin = ADMINBOT || config.ADMINBOT || [];
+  const listNDH = NDH || config.NDH ||  [];
+  {
+    const PREFIX = config.PREFIX;
+    const namebot = config.BOTNAME;
+    const { commands } = global.client;
+    const threadSetting = (await Threads.getData(String(event.threadID))).data || 
+    {};
+    const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX 
+    : global.config.PREFIX;
+    const dateNow = Date.now();
+    const time = process.uptime(),
+                      hours = Math.floor(time / (60 * 60)),
+                      minutes = Math.floor((time % (60 * 60)) / 60),
+                      seconds = Math.floor(time % 60);
+    const data = [
+      "Bạn không thể tìm được lệnh admin tại 'help' của MintBot",
+      "Đừng mong chờ gì từ MintBot.",
+      "Cái đoạn này á? Của SpermBot.",
+      "Nếu muốn không lỗi lệnh thì hãy xài những lệnh có trong help vì những lệnh lỗi đã bị ẩn rồi.",
+      "Đây là một con bot được các coder của MiraiProject nhúng tay vào.",
+      "Muốn biết sinh nhật của Mint thì hãy xài 'birthday'.",
+      "Cặc.",
+      "Cút.",
+      "Lồn.",
+      "Bạn chưa biết.",
+      "Bạn đã biết.",
+      "Bạn sẽ biết.",
+      "Không có gì là hoàn hảo, MintBot là ví dụ.",
+      "Mirai dropped.",
+      "MintBot là MiraiProject nhưng module là idea của SpermBot.",
+      "Bạn không biết cách sử dụng MintBot? Đừng dùng nữa.",
+      "Muốn chơi game? Qua bot khác mà chơi đây không rảnh",
+      "MintBot có thể hiểu phụ nữ nhưng không thể có được họ.",
+      "MintBot cân spam nhưng không có gì đáng để bạn spam."
+    ];
+    var link = [
+      "https://i.postimg.cc/KYyZK6yj/received-781804380019602.jpg",
+    ];
+
+    var i = 1;
+    var msg = [];
+    const moment = require("moment-timezone");
+    const date = moment.tz("Asia/Kolkata").format("HH:MM:ss L");
+    for (const idAdmin of listAdmin) {
+      if (parseInt(idAdmin)) {
+        const name = await Users.getNameUser(idAdmin);
+        msg.push(`${i++} ${name} - ${idAdmin}`);
+      }
+    }
+    var msg1 = [];
+            for (const idNDH of listNDH) {
+                if (parseInt(idNDH)) {
+                  const name1 = (await Users.getData(idNDH)).name
+                    msg1.push(`${i++} ${name1} - ${idNDH} `);
+                }
+            }
+    var callback = () => 
+      api.sendMessage({ body: `🌹𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍FO 🌹
+─────────────────\n♪♪♪♪♪♪♪『${namebot}』.♪♪♪♪♪♪♪\n─────────────────\n» Prefix system: ${PREFIX}\n» Prefix box: ${prefix}\n» Modules: ${commands.size}\n» Ping: ${Date.now() - dateNow}ms\n» Total users: ${global.data.allUserID.length} \n» Total threads: ${global.data.allThreadID.length} ─────────────────\n╭───────────╮\n🌻 𝙤𝙬𝙣𝙚𝙧 आदि बाबू🤸🌻\n╰───────────╯ ╭────────────╮\n🥀𝙗𝙤𝙩 𝗮𝗱𝗺𝗶𝗻 आदि बाबू 🤸  💞 \n╰────────────╯\n🍇𝙛𝙖𝙘𝙚𝙗𝙤𝙤𝙠 𝙖𝙘𝙘𝙤𝙪𝙣𝙩🍇\n───────https://www.facebook.com/profile.php?id=100010492052172\n─────────────────`, attachment: fs.createReadStream(__dirname + "/cache/kensu.jpg"), }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/kensu.jpg"));
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname + "/cache/kensu.jpg")).on("close", () => callback()); 
   }
 };
-module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
-const moment = require("moment-timezone");
-var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
-var link =                                     
-["https://i.imgur.com/eDbdlvd.jpg"];
-var callback = () => api.sendMessage({body:` ╾━╤デ╦︻(▀̿Ĺ̯▀̿ ̿)🇮🇳 𝐀𝐃𝐌𝐈𝐍 𝐀𝐍𝐃 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍 🇮🇳 
-(⌐▀͡ ̯ʖ▀)︻̷┻̿═━一-
-
-☄️Bot Name︎︎︎☄️  ${global.config.BOTNAME}
-
-🔥Bot Admin🔥☞︎︎︎☜︎︎︎✰ ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙💔🥀
-
-🙈bot andmin owner facebook id link🙈➪ www.facebook.com/priyanshu.rajput.official 💞🕊️
-
-👋For Any Kind Of Help Contact On Telegram  Username 👉 @Priyanshrajput😇
-
-✧══════•❁❀❁•══════✧
-
-🌸Bot Prefix🌸☞︎︎︎☜︎︎︎✰ ${global.config.PREFIX}
-
-♥️Bot Owner♥️ ☞︎︎︎☜︎︎︎✰ 𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭
-
-🥳UPTIME🥳
-
-🌪️Today is🌪️ ☞︎︎︎☜︎︎︎✰ ${juswa} 
-
-⚡Bot is running⚡ ${hours}:${minutes}:${seconds}.
-
-✅Thanks for using ${global.config.BOTNAME} Bot🖤
-
-
-🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
-┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
-🌸✦✧✧✧✧✰🍒ℙ𝕣𝕚𝕪𝕒𝕟𝕤𝕙🌿✰✧✧✧✧✦🌸
-┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛
-
-
-`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
-   };
