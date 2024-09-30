f323187f9509ac2c1fed5081fd3dae24564b9981
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-09-30 20:36:15 +0530

    Update commands

---
+++ Priyansh/commands/0_AADI_THANK-YOU.js
@@ -0,0 +1,31 @@
+//@Prem-babu3608
+////////////////////////////////////////////////////////
+/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
+const fs = require("fs");
+module.exports.config = {
+	name: "THANK YOU",
+    version: "1.1.1",
+	hasPermssion: 0,
+	credits: "PREM BABU", 
+	description: "THIS BOT IS PREM SHARMA",
+	commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("welcome") ||
+     react.includes("Welcome") || react.includes("WELCOME") || react.includes("स्वागत") ||
+react.includes("WELCOME") ||
+react.includes("Walcome")) {
+		var msg = {
+				body: `🙋  🅃🄷🄰🄽🄺  🅈🄾🅄  🙋`,
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🤩", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
