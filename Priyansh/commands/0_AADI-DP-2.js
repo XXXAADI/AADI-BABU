ca25200a73a596481098d0a093146dff8964a914
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-07 17:35:13 +0530

    Update 0_AADI-DP-2.js

--- Priyansh/commands/0_AADI-DP-2.js
+++ Priyansh/commands/0_AADI-DP-2.js
@@ -2,8 +2,8 @@ module.exports.config = {
   name: "dp2",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "PREM BABU",
-  description: "THIS BOT IS MADE BY PREM BABU",
+  credits: "AADI BABU",
+  description: "THIS BOT IS MADE BY AADI BABU",
   commandCategory: "PROFILE DP 2",
   usages: "SELF DP AND MENTION",
   dependencies: {
@@ -103,4 +103,4 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
     }
  
  
-//MADE BY PREM BABU 
+//MADE BY AADI BABU 
