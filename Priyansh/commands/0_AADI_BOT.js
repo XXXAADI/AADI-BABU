2085bc396d1919e4bd8216939833f0a57bf06b59
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-07 13:09:46 +0530

    Update 0_AADI_BOT.js

--- Priyansh/commands/0_AADI_BOT.js
+++ Priyansh/commands/0_AADI_BOT.js
@@ -139,10 +139,10 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
   
   if (event.body.indexOf("BOT") == 0 || (event.body.indexOf("BOT") == 0)) {
     var msg = {
-      body: `💙───${name}───💙\n\n                                                              ●=============================●
+      body: `•💜•${name}•💜•\n\n                                                              ●=============================●
 
                                                                                                                                                                                                                                                         ${rand}                                                                                             \n\n
-●=============================●`
+𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮`
     }
     return api.sendMessage(msg, threadID, messageID);
   };
