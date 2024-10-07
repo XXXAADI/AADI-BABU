8c8e1b2afa504cb1b39aa12bb14273b9c4690462
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-07 18:22:35 +0530

    Update 0_AADI_MISS-YOU.js

--- Priyansh/commands/0_AADI_MISS-YOU.js
+++ Priyansh/commands/0_AADI_MISS-YOU.js
@@ -1,13 +1,10 @@
-//@Prem-babu3608
-////////////////////////////////////////////////////////
-/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
 const fs = require("fs");
 module.exports.config = {
 	name: "MISS-YOU",
     version: "1.1.1",
 	hasPermssion: 0,
-	credits: "PREM BABU", ///////@prem-babu3608
-	description: "THIS BOT IS MR PREM BABU",
+	credits: "PREM BABU",
+	description: "THIS BOT IS MR AADI BABU",
 	commandCategory: "no prefix",
     cooldowns: 5, 
 };
