b11c286c1fba4485761ef73ca4c8c60936745f53
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-07 18:19:33 +0530

    Update 0_AADI_BYE.js

--- Priyansh/commands/0_AADI_BYE.js
+++ Priyansh/commands/0_AADI_BYE.js
@@ -1,13 +1,10 @@
-//@Prem-babu3608
-////////////////////////////////////////////////////////
-/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT PREM BABU
 const fs = require("fs");
 module.exports.config = {
         name: "BYE",
     version: "1.1.1",
         hasPermssion: 0,
-        credits: "PREM BABU", 
-        description: "THIS BOT IS PREM SHARMA",
+        credits: "AADI BABU", 
+        description: "THIS BOT IS AADI SHARMA",
         commandCategory: "no prefix",
     cooldowns: 5, 
 };
