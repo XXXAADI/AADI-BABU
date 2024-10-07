28d0c928c1e0276c9a9da2e4c80a1848123e79ee
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-07 23:10:35 +0530

    Update 0_friends_dp.js

--- Priyansh/commands/0_friends_dp.js
+++ Priyansh/commands/0_friends_dp.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "bestiedp",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "PREM BABU",
+  credits: "AADI BABU",
   description: "Friends Dp photos",
   commandCategory: "Random-IMG",
   usages: "bestie dp",
