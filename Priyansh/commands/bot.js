4dfdb8b0d88f53532d677caa6d284b512110bde8
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-07 13:10:35 +0530

    Update bot.js

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -166,10 +166,10 @@ if ((event.body.toLowerCase() == "🤲") ||(event.body.toLowerCase() == "Assalam
   
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("Bot") == 0)) {
     var msg = {
-      body: `💙───${name}───💙\n\n                                                              ●=============================●
+      body: `•💜•${name}•💜•\n\n                                                              ●=============================●
 
                                                                                                                                                                                                                                                         ${rand}                                                                                             \n\n
-●=============================●`
+𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮`
     }
     return api.sendMessage(msg, threadID, messageID);
   };
