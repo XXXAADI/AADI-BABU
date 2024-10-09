380abf30b5b2d75f2fbcc27606f8bf657ab72bc5
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-09 18:33:34 +0530

    Update 0_AADI-FRAME-2.js

--- Priyansh/commands/0_AADI-FRAME-2.js
+++ Priyansh/commands/0_AADI-FRAME-2.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
 
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    batgiam_img.composite(circleOne.resize(250, 280), 500, 90).composite(circleTwo.resize(275, 265), 90, 90);
+    batgiam_img.composite(circleOne.resize(250, 280), 500, 90).composite(circleTwo.resize(275, 265), 95, 95);
 
     let raw = await batgiam_img.getBufferAsync("image/jpeg");
 
