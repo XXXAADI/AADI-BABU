06bd7dd7e59437ff3fb147d9055e5447c530b037
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-09 11:35:48 +0530

    Update birthday.js

--- Priyansh/commands/birthday.js
+++ Priyansh/commands/birthday.js
@@ -1,1 +1,27 @@
-const _0x39d683=_0x5751;function _0x5751(_0x2cd803,_0x2f023d){const _0x1e8fdf=_0x1e8f();return _0x5751=function(_0x5751c3,_0x395284){_0x5751c3=_0x5751c3-0xa4;let _0x197449=_0x1e8fdf[_0x5751c3];return _0x197449;},_0x5751(_0x2cd803,_0x2f023d);}(function(_0x5e4401,_0x199e43){const _0x4c20a5=_0x5751,_0x186a7b=_0x5e4401();while(!![]){try{const _0x47c37b=parseInt(_0x4c20a5(0xb8))/0x1*(-parseInt(_0x4c20a5(0xa4))/0x2)+parseInt(_0x4c20a5(0xb5))/0x3+parseInt(_0x4c20a5(0xaf))/0x4*(parseInt(_0x4c20a5(0xb7))/0x5)+parseInt(_0x4c20a5(0xab))/0x6+parseInt(_0x4c20a5(0xaa))/0x7*(parseInt(_0x4c20a5(0xba))/0x8)+-parseInt(_0x4c20a5(0xa9))/0x9+-parseInt(_0x4c20a5(0xa7))/0xa;if(_0x47c37b===_0x199e43)break;else _0x186a7b['push'](_0x186a7b['shift']());}catch(_0x31fa10){_0x186a7b['push'](_0x186a7b['shift']());}}}(_0x1e8f,0xd440d),module[_0x39d683(0xb0)][_0x39d683(0xb6)]={'name':'birthday','version':_0x39d683(0xa5),'hasPermssion':0x0,'credits':'Mr\x20Chand','description':'','commandCategory':'0','cooldowns':0x5},module[_0x39d683(0xb0)][_0x39d683(0xb3)]=function({event:_0x35c137,api:_0x53a1c4}){const _0x11162b=_0x39d683,_0x4c456d=Date[_0x11162b(0xad)](_0x11162b(0xb4))-Date[_0x11162b(0xad)](new Date()),_0x2726d7=Math[_0x11162b(0xb2)](_0x4c456d/0x3e8%0x3c),_0x554e3a=Math['floor'](_0x4c456d/0x3e8/0x3c%0x3c),_0x592f46=Math[_0x11162b(0xb2)](_0x4c456d/(0x3e8*0x3c*0x3c)%0x18),_0xf15782=Math[_0x11162b(0xb2)](_0x4c456d/(0x3e8*0x3c*0x3c*0x18));return _0x53a1c4['sendMessage'](_0x11162b(0xb9)+_0xf15782+_0x11162b(0xac)+_0x592f46+_0x11162b(0xb1)+_0x554e3a+_0x11162b(0xae)+_0x2726d7+_0x11162b(0xa8),_0x35c137[_0x11162b(0xa6)],_0x35c137[_0x11162b(0xbb)]);});function _0x1e8f(){const _0x54a426=['\x20minutes\x20','22580OCzjtY','exports','\x20hours\x20','floor','run','March\x2015,\x202023\x2000:00:00','180075Gwutrm','config','1045xYzPub','1094zwrxTD','𝐂𝐇𝐀𝐍𝐃\x20𝐊𝐈\x20𝐁𝐈𝐑𝐓𝐇𝐃𝐀𝐘\x20𝐌𝐀𝐈𝐍\x20𝐉𝐔𝐒𝐓\x0a»\x20','28904EDGJDv','messageID','122Hphtus','1.0.0','threadID','1932910xAKrZl','\x20seconds«\x20𝐑𝐇𝐓𝐀\x20𝐇𝐀𝐈','15200829ghaSUe','217PMooaX','8799372YDoziS','\x20days\x20','parse'];_0x1e8f=function(){return _0x54a426;};return _h;}
+module.exports.config = {
+        name: "bday",
+        version: "1.0.0",
+        hasPermssion: 0,
+  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+        description: "See admin's birthday",
+  usePrefix: false,
+        commandCategory: "bday",
+        cooldowns: 5
+}
+
+module.exports.run =  ({ api, event, args, client, Users, Threads, __GLOBAL, Currencies }) => {
+  const axios = global.nodemodule["axios"];
+  const request = global.nodemodule["request"];
+  const fs = global.nodemodule["fs-extra"];
+    const t = Date.parse("June 27, 2024 00:00:00") - Date.parse(new Date());
+    const seconds = Math.floor( (t/1000) % 60 );
+    const minutes = Math.floor( (t/1000/60) % 60 );
+    const hours = Math.floor( (t/(1000*60*60)) % 24 );
+    const days = Math.floor( t/(1000*60*60*24) );
+    var callback = () => api.sendMessage(
+  {body:`Time left until Admin - Light birthday\n» ${days} days\n ${hours} hours\n ${minutes} minutes\n ${seconds} seconds. «`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+    fs.unlinkSync(__dirname + "/cache/1.png"));  
+      return request(encodeURI(`https://graph.facebook.com/100037743553265/picture?height=720&width=720&access_token=66262`)).pipe(
+fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
+
+      };
