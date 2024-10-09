34676ec35f435f437f3f8ed1bf0d13d563f22423
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-09 12:00:03 +0530

    Update birthday.js

--- Priyansh/commands/birthday.js
+++ Priyansh/commands/birthday.js
@@ -1,27 +1,37 @@
 module.exports.config = {
-        name: "bday",
-        version: "1.0.0",
-        hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-        description: "See admin's birthday",
-  usePrefix: false,
-        commandCategory: "bday",
-        cooldowns: 5
+
+ name: "birthday",
+
+ version: "1.0.0",
+
+ hasPermssion: 0,
+
+ credits: "Mr Chand",
+
+ description: "",
+
+ commandCategory: "0",
+
+ cooldowns: 5
+
 }
 
-module.exports.run =  ({ api, event, args, client, Users, Threads, __GLOBAL, Currencies }) => {
-  const axios = global.nodemodule["axios"];
-  const request = global.nodemodule["request"];
-  const fs = global.nodemodule["fs-extra"];
-    const t = Date.parse("June 27, 2024 00:00:00") - Date.parse(new Date());
-    const seconds = Math.floor( (t/1000) % 60 );
-    const minutes = Math.floor( (t/1000/60) % 60 );
-    const hours = Math.floor( (t/(1000*60*60)) % 24 );
-    const days = Math.floor( t/(1000*60*60*24) );
-    var callback = () => api.sendMessage(
-  {body:`Time left until Admin - Light birthday\n» ${days} days\n ${hours} hours\n ${minutes} minutes\n ${seconds} seconds. «`, attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
-    fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100037743553265/picture?height=720&width=720&access_token=66262`)).pipe(
-fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
 
-      };
+
+module.exports.run = function ({ event, api }) {
+
+    const t = Date.parse("March 15, 2023 00:00:00") - Date.parse(new Date());
+
+    const seconds = Math.floor( (t/1000) % 60 );
+
+    const minutes = Math.floor( (t/1000/60) % 60 );
+
+    const hours = Math.floor( (t/(1000*60*60)) % 24 );
+
+    const days = Math.floor( t/(1000*60*60*24) );
+
+
+
+    return api.sendMessage(`𝐂𝐇𝐀𝐍𝐃 𝐊𝐈 𝐁𝐈𝐑𝐓𝐇𝐃𝐀𝐘 𝐌𝐀𝐈𝐍 𝐉𝐔𝐒𝐓\n» ${days} days ${hours} hours ${minutes} minutes ${seconds} seconds« 𝐑𝐇𝐓𝐀 𝐇𝐀𝐈`, event.threadID, event.messageID);
+
+}
