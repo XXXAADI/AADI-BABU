e5dccb83c6579b8bfe9894fc9414948e58be5176
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-09 17:17:21 +0530

    Update 0_AADI-FRAME-2.js

--- Priyansh/commands/0_AADI-FRAME-2.js
+++ Priyansh/commands/0_AADI-FRAME-2.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'frame2.jpeg');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/a4ddL9o.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/U7Rblmo.jpeg", path);
 }
 
 async function makeImage({ one, two }) {
