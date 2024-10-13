e2efd3f088a51eccb85f027e5f79a7f194d91ad8
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-13 23:08:23 +0530

    Update 0_AADI_BOSS.js

--- Priyansh/commands/0_AADI_BOSS.js
+++ Priyansh/commands/0_AADI_BOSS.js
@@ -27,6 +27,6 @@ const fs = global.nodemodule["aadi"];
 "https://i.imgur.com/86kmX7S.jpeg",
 "https://i.imgur.com/jrYsBnF.jpeg"
      ];
-     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     प्रेम-बाबू                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nदेख लो मेरे बॉस प्रेम बाबू को सबसे अलग मासूम चेहरा भोली सी सूरत आखों में प्यार दिल में खुमार ♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     प्रेम-बाबू                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nदेख लो मेरे बॉस प्रेम बाबू को सबसे अलग मासूम चेहरा भोली सी सूरत आखों में प्यार दिल में खुमार ♥️`,attachment: fs.createReadStream(__dirname + "/cache', '1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache', '1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
