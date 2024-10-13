89c038463f5ee97206720feca4907c7132eabcb2
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-13 22:57:40 +0530

    Update 0_AADI+AADI.js

--- Priyansh/commands/0_AADI+AADI.js
+++ Priyansh/commands/0_AADI+AADI.js
@@ -2,10 +2,10 @@ module.exports.config = {
   name: "AadI",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "AaDI BABU",
-  description: "THIS BOT IS MADE BY AaDI BABU",
-  commandCategory: "AaDI BABU IMAGE",
-  usages: "AaDI-BABU",
+  credits: "AADI BABU",
+  description: "THIS BOT IS MADE BY AADI BABU",
+  commandCategory: "AADI BABU IMAGE",
+  usages: "AADI-BABU",
   cooldowns: 2,
   dependencies: {
     "request":"",
@@ -16,17 +16,17 @@ module.exports.config = {
 };
 
 module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
-const axios = global.nodemodule["AADI"];
+const axios = global.nodemodule["boss"];
 const request = global.nodemodule["Aadi"];
 const fs = global.nodemodule["aadi"];
     var link = [
 "https://i.imgur.com/I9cM0yI.jpeg",
-"https://i.imgur.com/1MrF4bm.jpeg",
 "https://i.imgur.com/8fIaObC.jpeg",
 "https://i.imgur.com/WxJyNrf.jpeg",
-"https://i.imgur.com/aVKNL0y.jpeg",
-"https://i.imgur.com/BJrkdEB.jpeg"
+"https://i.imgur.com/1MrF4bm.jpeg",
+"https://i.imgur.com/BJrkdEB.jpeg",
+"https://i.imgur.com/aVKNL0y.jpeg"
      ];
-     var callback = () => api.sendMessage({body:`● ========================= ●\n=𝐎𝐰𝐧𝐞𝐫 ➻    🥀 𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 🥀\n● ========================= ●\n\n● ======= 𝐇𝐄𝐋𝐋𝐎 𝐁𝐀𝐁𝐘 ======= ●`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     प्रेम-बाबू                    ✧═══•❁😛❁•═══✧\n┗━━━━━┛\n\nदेख लो मेरे बॉस प्रेम बाबू को सबसे अलग मासूम चेहरा भोली सी सूरत आखों में प्यार दिल में खुमार ♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
    };
