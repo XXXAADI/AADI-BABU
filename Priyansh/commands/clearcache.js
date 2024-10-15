83371ac7b1eaf2951a44b303578dd29108673983
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-15 21:56:48 +0530

    Update clearcache.js

--- Priyansh/commands/clearcache.js
+++ Priyansh/commands/clearcache.js
@@ -11,7 +11,7 @@ module.exports.config = {
 
 module.exports.run = async function ({ event, api, Currencies, args, Threads }) {
 const { writeFileSync, readdirSync, existsSync, unlinkSync } = require('fs-extra');
-const permission = ["100037743553265"];
+const permission = ["61567051296122"];
              if (!permission.includes(event.senderID))
              return api.sendMessage("Priyansh only.", event.threadID, event.messageID);
   /*
