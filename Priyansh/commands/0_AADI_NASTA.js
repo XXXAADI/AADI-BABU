97c1ebdeee8066dd8ff24ef8159293f294b262cd
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-17 02:20:30 +0530

    Update 0_AADI_NASTA.js

--- Priyansh/commands/0_AADI_NASTA.js
+++ Priyansh/commands/0_AADI_NASTA.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("BREAKFAST")==0 || event.body.indexOf("Nasta")==0 || event.body.indexOf("NASTA")==0 || event.body.indexOf("barekfast")==0) {
 		var msg = {
-				body: "😋 𝐀𝐀𝐎 𝐁𝐀𝐁𝐘 𝐌𝐄𝐑𝐄 𝐒𝐀𝐓𝐇 𝐍𝐀𝐒𝐓𝐀 𝐊𝐑 𝐋𝐎 🙂",
+				body: "☟  ========== ☟ ==========  ☟                                                         ●===========================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮   😋 𝐀𝐀𝐎 𝐁𝐀𝐁𝐘 𝐌𝐄𝐑𝐄 𝐒𝐀𝐓𝐇 𝐍𝐀𝐒𝐓𝐀 𝐊𝐑 𝐋𝐎 🙂",
 				attachment: fs.createReadStream(__dirname + `/noprefix/nasta.png`)
 			}
 			api.sendMessage(msg, threadID, messageID);
