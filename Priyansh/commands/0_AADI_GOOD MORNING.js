fac2e9b743b404d3e9794445954c50ee56dc7a07
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-16 08:43:40 +0530

    Update 0_AADI_GOOD MORNING.js

--- Priyansh/commands/0_AADI_GOOD MORNING.js
+++ Priyansh/commands/0_AADI_GOOD MORNING.js
@@ -15,7 +15,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	if (event.body.indexOf("GM")==0 || event.body.indexOf("Gm")==0 || event.body.indexOf("Good morning")==0 || event.body.indexOf("GOOD MORNING")==0) {
 		var msg = {
 				body: "😁 𝐆𝐎𝐎𝐃 𝐌𝐎𝐑𝐍𝐈𝐍𝐆 𝐃𝐀𝐑𝐋𝐈𝐍𝐆 😁",
-				attachment: fs.createReadStream(__dirname + `/noprefix/20240808_084237.jpg`)
+				attachment: fs.createReadStream(__dirname + `/noprefix/received_1083155713406602.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😘", event.messageID, (err) => {}, true)
