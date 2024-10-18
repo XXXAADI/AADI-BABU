0d75bc2e46d6dfec63f7d1dcd5ea5d49d2a0a9b5
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-19 04:02:38 +0530

    Update commands

---
+++ Priyansh/commands/0_AADI-THARKI.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+module.exports.config = {
+        name: "TharKi",
+    version: "1.1.1",
+        hasPermssion: 0,
+        credits: "AADI BABU", 
+        description: "THIS BOT IS AADI SHARMA",
+        commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+        var { threadID, messageID } = event;
+        let react = event.body.toLowerCase();
+        if(react.includes("THARKI") ||
+     react.includes("Tharki") || react.includes("tharki") || react.includes("𝐓𝐇𝐀𝐑𝐊𝐈") ||
+react.includes("𝚃𝙷𝙰𝚁𝙺𝙸") ||
+react.includes(" ＴＨＡＲＫＩ")) {
+                var msg = {
+                                body: `𝐓𝐔 𝐓𝐇𝐀𝐑𝐊𝐈 𝐓𝐄𝐑𝐈 𝐆𝐅 𝐓𝐇𝐀𝐑𝐊𝐈 𝐓𝐄𝐑𝐄 𝐀𝐀𝐍𝐄 𝐖𝐀𝐋𝐄 𝐁𝐀𝐂𝐇𝐇𝐄 𝐓𝐇𝐀𝐑𝐊𝐈  😏😏😼😼😼`,
+                        }
+                        api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("😆", event.messageID, (err) => {}, true)
+                }
+        }
+        module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
