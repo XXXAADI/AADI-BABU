3b9427a91c629179c3a9b2f775f7fde887715cc8
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-22 21:52:28 +0530

    Update commands

---
+++ Priyansh/commands/0_AADI-CHICKEN.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+        name: "chicken",
+    version: "1.0.1",
+        hasPermssion: 0,
+        credits: "AADI BABU", 
+        description: "hihihihi",
+        commandCategory: "no prefix",
+        usages: "chicken",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+        var { threadID, messageID } = event;
+        if (event.body.indexOf("chicken")==0 || event.body.indexOf("Chicken")==0 || event.body.indexOf("CHICKEN")==0 || event.body.indexOf("CHIKAN")==0) {
+                var msg = {
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐡𝐢𝐜𝐇𝐞𝐍\n__________________________________ ",
+                                attachment: fs.createReadStream(__dirname + `/noprefix/62306739ed186731b1f646029335e853.jpg`)
+                        }
+                        api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🐔", event.messageID, (err) => {}, true)
+                }
+        }
+        module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+        }
