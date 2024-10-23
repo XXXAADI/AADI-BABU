7d8ba4bfa948818d69fe20c47dc44bc2f87ac961
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-23 15:47:52 +0530

    Update 0_AADI_GOOD MORNING.js

--- Priyansh/commands/0_AADI_GOOD MORNING.js
+++ Priyansh/commands/0_AADI_GOOD MORNING.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("GM")==0 || event.body.indexOf("Gm")==0 || event.body.indexOf("Good morning")==0 || event.body.indexOf("GOOD MORNING")==0) {
 		var msg = {
-				body: "☟  ========== ☟ ==========  ☟                                                         ●===========================●                              𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐀𝐚̃𝐃𝐢   𝐁𝐚𝐁𝐮  🩷 ⎯꯭֯🌸⃪ ꯭⃛֯G❍❍𝐃 ɱ❍ɽηIIηG ♥️...🤍EvEry❍ηE🤍...♥️ 🩷",
+				body: "body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n⎯꯭֯🌸⃪ ꯭⃛֯G❍❍𝐃 ɱ❍ɽηIIηG EvEry❍ηE \n__________________________________ ",",
 				attachment: fs.createReadStream(__dirname + `/noprefix/received_1083155713406602.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
