7888affdf93481bfbd62e3b3d33fa48cb8e7108a
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-23 17:03:11 +0530

    Update commands

---
+++ Priyansh/commands/0_AADI-PARATHE.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+        name: "parathe",
+    version: "1.0.1",
+        hasPermssion: 0,
+        credits: "AADI BABU", 
+        description: "hihihihi",
+        commandCategory: "no prefix",
+        usages: "parathe",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+        var { threadID, messageID } = event;
+        if (event.body.indexOf("parathe")==0 || event.body.indexOf("paratha")==0 || event.body.indexOf("PARATHE")==0 || event.body.indexOf("Parathe")==0) {
+                var msg = {
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐚𝐑𝐚𝐓𝐡𝐞\n__________________________________ ",
+                                attachment: fs.createReadStream(__dirname + `/noprefix/7688e35b3d9376764427668db625967c.jpg`)
+                        }
+                        api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🫔", event.messageID, (err) => {}, true)
+                }
+        }
+        module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+        }
