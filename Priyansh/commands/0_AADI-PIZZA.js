1d93bd7d279a8ab4397ac856ef40b8afe3b78e89
Ɓɘwʌʆʌ Boɭtīī Pʋbɭīīɕ <asingh12131415161718@gmail.com>
2024-10-23 09:32:04 +0530

    Update and rename 0_AADI-PIZZA.JA to 0_AADI-PIZZA.js

--- Priyansh/commands/0_AADI-PIZZA.js
+++ Priyansh/commands/0_AADI-PIZZA.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "pizza",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "FAIZ ANSARI", 
+        credits: "AADI BABU", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "pizza",
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pizza")==0 || event.body.indexOf("Pizza")==0 || event.body.indexOf("PIZZA")==0 || event.body.indexOf("PIZZAA")==0) {
                 var msg = {
-                                body: "😁𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐏𝐈𝐙𝐙𝐀🤟😁",
-                                attachment: fs.createReadStream(__dirname + `/noprefix/ffae4b570c7cab89fa416e43b56056f9.jpg`)
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐈𝐙𝐙𝐀 \n__________________________________ ",
+                                attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-Abhijeet-Agnihotri-pizza.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🍕", event.messageID, (err) => {}, true)
